aa6f199b9a858fdc8a48ad14871ad37682663b75
KOITT <KOITT@KOITT-PC>
2018-06-12 21:45:22 +0900

    6.12

--- ch04/src/ch04/Ex02_05.java
+++ ch04/src/ch04/Ex02_05.java
@@ -4,10 +4,19 @@ import java.util.*;
 
 public class Ex02_05 {
 	public static void main(String[] args) {
+		while(true) {
+			
 		Scanner scanner = new Scanner(System.in);
 		int num = Integer.parseInt(scanner.nextLine());
 		
-		while(num%3) {
+		int result = num/3;
+		if(num%3==0 ) {
+			System.out.println(result);
+		}else if(result == -1){
+			break;
+			
+		}
+		
 			
 		}
 	}
